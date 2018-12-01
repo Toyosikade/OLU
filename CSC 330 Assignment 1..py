27d5d24eda714f4c0744564962726d35598a4de1
Oluwatoyosi <toyosikade@gmail.com>
2018-11-30 21:27:13 -0600

    Add files via upload
    This code snippet was an assignment for a class, and it involves the implementation of List in python and how i condensed what would have been multiple print statements into 1 print statement all in 1 line.

---
+++ CSC 330 Assignment 1..py
@@ -0,0 +1,22 @@
+###################################################################################################################
+#Name: Oluwatoyosi Kade
+#Class: CSC 330
+#Date: 11/30/2018
+#
+#Code Description:    This code prints out a list of languages and how proficient, familiar or marginal i am in them. to compile
+#and execute this python code, the source code is saved and then Python 'compiles' it when it runs, it is also passed to a python intepreter and
+#it will run compiling itself to bytecode. 
+####################################################################################################################
+
+Compiled = [ "C", "C++"]
+print ("For compiled languages i am PROFICIENT in" , Compiled[0], "and FAMILIAR with", Compiled[1]  )
+
+print ("             ")
+
+Interpreted = ["Python", "PHP", "JavaScript"]
+print ("For interpreted languages i am PROFICIENT in", Interpreted[0], "but, i have no FAMILIARITY or MARGINABILITY with PHP or JavaScript")
+
+print ("            ")
+
+Hybrid = ["JAVA", "Perl", "bash"]
+print ("For Hybrid languages i am FAMILIAR with", Hybrid[0], "and MARGINAL in ", Hybrid[2])
